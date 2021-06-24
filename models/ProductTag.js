df8b339bc97cee409777c4e302fbabbdcea5c84c
eweagraff <weagraff.emily@gmail.com>
2021-06-24 09:08:37 -0700

    added the models for category, product, tag and product tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define the columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      //reference the product and id models
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //reference the tag and models id
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
